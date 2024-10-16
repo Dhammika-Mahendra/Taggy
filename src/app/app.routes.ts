beb83a2f2cc56375cdb5b673e673de6af992e9fb
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-10-16 18:36:41 +0530

    Basic email password fileds validation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LogInComponent } from './components/auth/log-in/log-in.component';
+import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
+import { AuthComponent } from './components/auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login', component: LogInComponent},
+    {path:'signup', component: SignUpComponent},
+    {path:'auth', component: AuthComponent }
+];
--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule,MatInputModule,MatButtonModule,MatIconModule,CommonModule,FormsModule,ReactiveFormsModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
+  loginForm!: FormGroup; 
+  submitLabel : string  = 'Log In';
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
 
+  constructor() {}
+
+  getEmailErrorMessage() {
+    if(this.email?.hasError('required')){
+      return 'You must enter a value';
+    }
+    if(this.email?.hasError('email')){
+      return 'Not a valid email';
+    }
+    return '';
+  }
+
+  getPasswordErrorMessage() {
+    if(this.password?.hasError('required')){
+      return 'You must enter a password';
+    }
+    return '';
+  }
+
+  onSubmit(){
+
+  }
+ 
 }
